af2a52e925eb79946005743c271481f310cf53fb
Sebastien Kerbrat <sebastien@piktiv.se>
2018-09-15 15:46:24 +0200

    Change experiment scripts to python :

--- celery_runner.py
+++ celery_runner.py
@@ -1,33 +1,78 @@
-from celery import Celery
 import os
+import signal
 import subprocess
+from functools import partial
+
+from celery import Celery, signals, apps
+from celery.exceptions import WorkerTerminate
+from celery.platforms import EX_FAILURE
+from celery.signals import worker_shutdown, worker_shutting_down
+from celery.task.control import inspect
+
 app = Celery('tasks', broker='pyamqp://guest@localhost//')
 EXPERIMENTS_FOLDER = "./experiments/"
+MAIN_TASK_ID = 'main_task'
+
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(5.0, schedule.s(), name='Check experiments every second')
 
 
+@signals.celeryd_after_setup.connect
+def worker_ready(*args, **kwargs):
+    try:
+        apps.worker.install_worker_term_handler = partial(
+            apps.worker._shutdown_handler,
+            sig='SIGINT',
+            how='Cold',
+            exc=WorkerTerminate,
+            exitcode=EX_FAILURE
+        )
+    except Exception as e:
+        print(e)
+
+
 @app.task
-def schedule():    
-    for file in os.listdir(EXPERIMENTS_FOLDER):
-        if file.endswith("pending.sh"):
-            split = file.split('.')
-            experiment_name = split[0]
-            print('Found an experiment, running ', experiment_name)
-            os.rename(EXPERIMENTS_FOLDER + file, EXPERIMENTS_FOLDER + experiment_name + ".running.sh")
-            run_shell_file.delay(experiment_name)
-              
+def schedule():
+    tasks = list(inspect().active().values())[0]
+    run_shell_file_tasks = [x for x in tasks if x['type'] == 'celery_runner.run_shell_file']
+    if len(run_shell_file_tasks) == 0:
+        for file in os.listdir(EXPERIMENTS_FOLDER):
+            if file.endswith("pending.sh") and len(run_shell_file_tasks) == 0:
+                split = file.split('.')
+                experiment_name = split[0]
+                print('Found an experiment, running ', experiment_name)
+                os.rename(EXPERIMENTS_FOLDER + file, EXPERIMENTS_FOLDER + experiment_name + ".running.sh")
+                run_shell_file.apply_async(args=[experiment_name], task_id=MAIN_TASK_ID)
+
+
 @app.task
 def run_shell_file(experiment_name):
     script_path = os.path.abspath(EXPERIMENTS_FOLDER + experiment_name + ".running.sh")
     print('Running ', script_path)
-    with open('./outputs/' + experiment_name + ".log", 'w') as output:  
-        exit_code = subprocess.call([script_path], cwd='/mnt/bigdisk/sebastien/ailab', stdout = output, stderr=output)
-    if exit_code == 0:    
+    with open('./outputs/' + experiment_name + ".log", 'w') as output:
+        process = subprocess.Popen([script_path], cwd='/home/sphax/Desktop/ML/Experiments/experiment-runner/repositories/ppo', stdout=output, stderr=output, preexec_fn=os.setsid)
+
+    def signal_handler(sig, frame):
+        print('Received SIGINT, killing process')
+        os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+        print('Killed process')
+
+    signal.signal(signal.SIGTERM, signal_handler)
+    process.wait()
+    if process.returncode == 0:
         os.rename(script_path, EXPERIMENTS_FOLDER + experiment_name + ".finished.sh")
     else:
         os.rename(script_path, EXPERIMENTS_FOLDER + experiment_name + ".failed.sh")
-    
 
+@worker_shutdown.connect
+def shutdown(**kwargs):
+    print("Worker shutdown")
+    app.control.revoke(MAIN_TASK_ID, terminate=True, signal='SIGTERM')
+
+
+@worker_shutting_down.connect
+def shutting_down(**kwargs):
+    print("Worker shutting down")
+    app.control.revoke(MAIN_TASK_ID, terminate=True, signal='SIGTERM')
--- create_experiment.py
+++ create_experiment.py
@@ -5,22 +5,50 @@ Usage:
 
 """
 import os
-from docopt import docopt
+import subprocess
+from datetime import datetime
 
 
-if __name__ == '__main__':
-    arguments = docopt(__doc__)
-    print(arguments)
-    experiment_name = arguments['<name>']
-    file = arguments['<file>']
+def execute_shell_command(command, location):
+    exit_code = subprocess.call([command], cwd=location, shell=True)
+    if exit_code != 0:
+        raise RuntimeError
 
+
+def create_experiment_file(experiment_name, command):
     text = """#!/bin/bash
 set -e
 git fetch
 git checkout {0}
-python3 {1}
-    """.format(experiment_name, file)
+{1}
+        """.format(experiment_name, command)
     print(text)
-    with open('{0}.pending.sh'.format(experiment_name), 'w') as meta_file:
-      meta_file.write(text)
+    file_name = '{0}.pending.sh'.format(experiment_name)
+    with open(file_name, 'w') as meta_file:
+        meta_file.write(text)
+    os.chmod(file_name, 0o770)
+    return file_name
 
+
+def push_experiment(workspace_location, runner_location, name, command):
+    print("Experiment name: ", name)
+    print("File to run: ", command)
+
+    experiment_name = 'exp_{0}_{1}'.format(name, datetime.now().strftime("%m%d-%H%M"))
+    print("Branch name: ", experiment_name)
+
+    execute_shell_command('git checkout -b {0}'.format(experiment_name), workspace_location)
+    execute_shell_command('git add .', workspace_location)
+    execute_shell_command('git commit -m "Auto experiment: {0}"'.format(name), workspace_location)
+    execute_shell_command('git push origin {0}'.format(experiment_name), workspace_location)
+
+    experiment_file_name = create_experiment_file(experiment_name, command)
+    os.rename(experiment_file_name, runner_location + '/experiments/' + experiment_file_name)
+
+    execute_shell_command('git checkout master', workspace_location)
+    execute_shell_command('git reset --hard {0}'.format(experiment_name), workspace_location)
+    execute_shell_command('git reset HEAD~'.format(experiment_name), workspace_location)
+
+
+if __name__ == '__main__':
+    push_experiment('/home/sphax/Desktop/ML/Experiments/ppo', '/home/sphax/Desktop/ML/Experiments/experiment-runner', 'ce_main_test', 'python3 my_ppo.py Test FixedPosButton-v0')
